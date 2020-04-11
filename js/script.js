fd479b1cdf81b7c2085e15b64ebe4ada47bc376b
Maya Roman <maya.roman7@gmail.com>
2020-04-10 22:08:28 -0500

    Change background color and font family

--- js/script.js
+++ js/script.js
@@ -6,32 +6,7 @@ project 1 - A Random Quote Generator
 // For assistance: 
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
 // ------------------------------------------------------------
-
-// Create an Array of objects to store the data for your quotes
-
-// A data structure is necessary to store and organize the quotes in your app. A basic array of objects is a lightweight way to store information.
-  
-// In your js/script.js file:
-  
-  // - Create a variable named quotes and set it equal to an empty array.
-  
-  // - Add a minimum of five empty objects to your quotes array.
-
-// ------------------------------------------------------------
-
-// Add data to your quote objects
-
-// The objects in the quotes array store the individual properties of the quotes.
-
-// Add the following properties to each quote object:
-  // quote - string - the actual quote
-  // source - string - the person or character who said it
-  // Add a citation property to at least one quote object. The value should be a string holding a reference to the source of the quote, like the book, movie or song where the quote originates.
-  // Add a year property to at least one quote object. The value should be a string or number representing the year the quote originated.
-  
-// -------------------------------------------------------------
 
 /*** 
  * `quotes` array 
@@ -100,10 +75,8 @@ const printQuote = () => {
     
   }
   
-  
   document.querySelector(`#quote-box`).innerHTML = htmlString;
 };
-
 
 /***
  * click event listener for the print quote button
